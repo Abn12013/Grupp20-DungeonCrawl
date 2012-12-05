9446b7bdc04dff4d5495e1f5fb4bfbf3d5e77087
Albin Billman <Albin.Billman@gmail.com>
2012-12-05 13:32:02 +0100

    Revert "Revert "Bytte version till 0.0.4""
    This reverts commit a97e15e809721158c011d7cd125c48011d773412.

--- DungeonCrawl 0.0.4/DungeonCrawl/DungeonCrawl/Position.cs
+++ DungeonCrawl 0.0.4/DungeonCrawl/DungeonCrawl/Position.cs
@@ -16,5 +16,8 @@ namespace DungeonCrawl
 {
     class Position : Character
     {
+
+        
+
     }
 }
--- DungeonCrawl 0.0.4/DungeonCrawl/DungeonCrawl/PositionManager.cs
+++ DungeonCrawl 0.0.4/DungeonCrawl/DungeonCrawl/PositionManager.cs
@@ -7,5 +7,7 @@ namespace DungeonCrawl
         public int iteration; //iterationsnummer för objektet
         public int hp; //hälsa av objekt
         //Lägg till fler värden efter behov
+
+
     }
 }
